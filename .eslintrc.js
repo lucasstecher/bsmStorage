f54d715b233f4187559acbb77eb64f446fe88646
Lucas V. Stecher <lucasvieira0070@gmail.com>
2024-01-25 19:48:39 -0300

    :heavy_plus_sign: add and update dependencies and its files

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+const { off } = require("process");
+
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": "standard-with-typescript",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": ["./tsconfig.json"]
+    },
+    "rules": {
+        "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/prefer-nullish-coalescing": "off",
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "@typescript-eslint/explicit-function-return-type": "warn",
+        "@typescript-eslint/semi": "off",
+        "@typescript-eslint/return-await": "off",
+        "@typescript-eslint/space-before-function-paren": "off",
+        "@typescript-eslint/restrict-template-expressions": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/indent": "off",
+        "@typescript-eslint/promise-function-async": "off",
+        "@typescript-eslint/member-delimiter-style": "off",
+        "semi": "off",
+        'no-console': 'warn',
+    }
+}
